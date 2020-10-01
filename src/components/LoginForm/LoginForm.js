15e9125e9a96ea8c2f2939dc55b57e4467f90e1f
Russ <18russjames@gmail.com>
2020-10-01 12:45:45 -0700

    refactoring

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,3 +1,4 @@
+import TokenService from '../../services/token-service'
 import React, { Component } from 'react'
 import { Button, Input } from '../Utils/Utils'
 
@@ -12,8 +13,9 @@ export default class LoginForm extends Component {
     ev.preventDefault()
     const { user_name, password } = ev.target
 
-    console.log('login form submitted')
-    console.log({ user_name, password })
+    TokenService.saveAuthToken(
+      TokenService.makeBasicAuthToken(user_name.value, password.value)
+    )
 
     user_name.value = ''
     password.value = ''
